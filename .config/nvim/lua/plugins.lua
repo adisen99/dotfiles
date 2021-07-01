bc913ae750f24ba6042846d967270c2eeabbdc99
adisen99 <adityasenguptasai99@gmail.com>
2021-07-01 17:53:37 +0530

    :hammer_and_wrench: changed packer to optional plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,16 +3,22 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   execute 'packadd packer.nvim'
 end
 
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
 return require('packer').startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use {
+		'wbthomason/packer.nvim',
+		opt = true
+}
 
 	-- colorscheme
 	-- use 'metalelf0/jellybeans-nvim' -- jellybeans
